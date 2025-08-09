0a38f18b9a3b6a1cf011fe48b6a038b0c48184bb
Benjamin Graetz <benjamin.graetz@henleyhs.sa.edu.au>
2025-08-09 12:03:47 +0930

    Added FAQ applet

---
+++ apps/faq/script.js
@@ -0,0 +1,16 @@
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(button => {
+  button.addEventListener('click', () => {
+    const answer = button.nextElementSibling;
+    if (answer.style.maxHeight) {
+      // Close if open
+      answer.style.maxHeight = null;
+    } else {
+      // Close any open answers
+      document.querySelectorAll('.faq-answer').forEach(a => a.style.maxHeight = null);
+      // Open clicked one
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    }
+  });
+});
